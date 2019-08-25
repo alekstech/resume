17fb98d5ee964e1afcdb209642ae5c3bb02d438e
Aleks Sobieraj <contact@aleks.tech>
2019-08-25 01:40:15 -0400

    inline svg

--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import printMe from './print.js';
 import key from './key.svg';
 import Svg from './key.svg';
 
-
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = Svg;
-
-  return element;
+function header() {
+  const node = document.createElement('a');
+  node.setAttribute("href", "http://example.com")
+  const text = document.createTextNode("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum nisi esse expedita illum et quo sit iste officiis distinctio alias! Obcaecati unde dolor a voluptatum aut, maxime dicta expedita repellat.");
+  node.appendChild(text);
+  node.appendChild(svg());
+  return node;
 }
 
-document.body.appendChild(component());
+function anchor() {
+
+}
+
+function svg() {
+  const container = document.createElement('div');
+  container.innerHTML = Svg;
+  const node = container.firstChild;
+  return node;
+}
+
+function components() {
+  return [header]
+}
+
+function render() {
+  for (const component of components()) {
+    document.body.appendChild(component());
+  }
+};
+
+render();
